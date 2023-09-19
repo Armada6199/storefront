6fba52ad43d71de64cc06fd753724d7eebadad35
Armada6199 <moh.abdin27@gmail.com>
2023-09-19 09:55:07 +0300

    s

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -65,4 +65,92 @@ const mapDispatchToProps = {
     setProducts,
     setRenderList
 }
-export default connect(mapStateToProps, mapDispatchToProps)(ProductList)
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList)import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Stack } from '@mui/material';
+import axios from 'axios';
+import {
+  setActiveCategory,
+  setCategories,
+  setProducts,
+  setRenderList,
+} from '../../Reducers/CategoryReducer';
+import Product from './Product';
+
+function ProductList(props) {
+  useEffect(() => {
+    async function fetchCategoriesAndProducts() {
+      try {
+        const [categoriesResponse, productsResponse] = await Promise.all([
+          axios.get(`https://api-js401.herokuapp.com/api/v1/categories`),
+          axios.get(`https://api-js401.herokuapp.com/api/v1/products`),
+        ]);
+
+        props.setCategories(categoriesResponse.data.results);
+        props.setProducts(productsResponse.data.results);
+        props.setActiveCategory('electronics');
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchCategoriesAndProducts();
+  }, [props.setCategories, props.setProducts, props.setActiveCategory]);
+
+  useEffect(() => {
+    async function setToRenderProducts() {
+      try {
+        const toRender = props.productManager.categoryReducer.products.filter(
+          (element) =>
+            element.category === props.productManager.categoryReducer.activeCategory
+        );
+        props.setRenderList(toRender);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setToRenderProducts();
+  }, [
+    props.productManager.categoryReducer.activeCategory,
+    props.productManager.categoryReducer.products,
+    props.setRenderList,
+  ]);
+
+  return (
+    <div style={{ minHeight: '70vh' }}>
+      <Stack
+        direction={'row'}
+        gap={'15px'}
+        justifyContent='center'
+        flexWrap={'wrap'}
+        width='80%'
+        margin={'auto'}
+        marginY={'50px'}
+      >
+        {props.productManager.categoryReducer.toRender.map((product) => (
+          <Product
+            key={product['_id']}
+            name={product.name}
+            category={product.category}
+            inStock={product.inStock}
+            price={product.price}
+            product={product}
+          />
+        ))}
+      </Stack>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  productManager: state,
+});
+
+const mapDispatchToProps = {
+  setActiveCategory,
+  setCategories,
+  setProducts,
+  setRenderList,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
